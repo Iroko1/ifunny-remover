4908b8bdddac24c7516d72109142a937fdf175c7
Totokori <totokori1@gmail.com>
2022-11-14 20:54:32 -0600

    Better comments

--- main.py
+++ main.py
@@ -14,14 +14,16 @@ while(True):
             input = Image.open(infile)
             width, height = input.size
             output = input.crop((0, 0, width, (height - 20)))
-
+            
+            # remove this section if you want it in the same folder
             if not os.path.exists('output'):
                 os.makedirs('output')
             output.save(infile)
-            #made better for google photos
+            #made better for google photos sync
             os.rename(infile,'output/' + infile)
             
             # Adds the tag so that it doesn't get cropped again
+            #remove 'output/' if you want it in the same folder
             with open('output/' + infile, 'rb') as img_file:
                 image = img(img_file)
             image.set('user_comment', 'True')
